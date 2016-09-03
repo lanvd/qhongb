b2775b3c94b9bc20a3208bf0b80c577eaef8f90c
lanwl <lanwl@163.com>
2016-09-03 17:27:06 +0800

    aaa

---
+++ src/com/example/qhongb/ConfigStr.java
@@ -0,0 +1,11 @@
+package com.example.qhongb;
+
+public class ConfigStr {
+	public static String S_INPUT_TEXT ="com.tencent.mm:id/z4";
+	public static String S_SEND_BUTTON ="com.tencent.mm:id/z_";
+	public static String S_HONG_BAO ="com.tencent.mm:id/a13";
+	public static String S_CHAT_ITME ="com.tencent.mm:id/h";
+	public static String S_CHAT_ITME_PERSON ="com.tencent.mm:id/h6";
+	public static String S_CHAT_ITME_CONTENT ="com.tencent.mm:id/h7";
+	public static String S_CHAT_ITME_IMAGE ="com.tencent.mm:id/h5";
+}
